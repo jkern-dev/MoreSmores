8cd6578ad43d536b038657376e89513b9d7aa904
Josh Kern <jkern.dev@gmail.com>
2019-06-04 16:21:42 -0700

    update navbar for bookings

--- frontend/components/greeting/greeting.jsx
+++ frontend/components/greeting/greeting.jsx
@@ -13,7 +13,6 @@ const Greeting = ({ currentUser, logout, openModal }) => {
                     <ul>
                         <li><Link to='/sites' className="header-link">Camp</Link></li>
                         <li><Link onClick={()=>openModal('login')} className="header-link">Host</Link></li>
-                        {/* <li ><Link to='/' className="header-link">Help</Link></li> */}
                         <li><Link onClick={() => openModal('login')} className="header-link">Login</Link></li>
                         <li><Link onClick={() => openModal('signup')} className="header-link">Sign Up</Link></li>
                     </ul>
@@ -32,7 +31,6 @@ const Greeting = ({ currentUser, logout, openModal }) => {
                         <li><Link to='/sites' className="header-link">Camp</Link></li>
                         <li><Link to='/site_create' className="header-link">Host</Link></li>
                         <li><Link to={`/bookings/${currentUser.id}`} className="header-link">Bookings</Link></li>
-                        {/* <li ><Link to='/' className="header-link">Help</Link></li> */}
                         <div className="dropdown">
                           <li className="header-link">
                               {currentUser.first_name.charAt(0).toUpperCase() + currentUser.first_name.slice(1)}
