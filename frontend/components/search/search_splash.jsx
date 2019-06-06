47002296967c107cffee2f15622c4d7c7ba18b0a
Josh Kern <jkern.dev@gmail.com>
2019-06-06 10:24:20 -0700

    search bar redirecting

--- frontend/components/search/search_splash.jsx
+++ frontend/components/search/search_splash.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom';
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCampground, faBus, faDog, faCalendarAlt } from '@fortawesome/free-solid-svg-icons';
@@ -9,6 +10,14 @@ library.add(faCalendarAlt);
 
 
 class SplashSearch extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchValue: ""
+    };
+    this.submitSearch = this.submitSearch.bind(this);
+  }
+
   flipCalendar() {
     if (document.getElementById("anytime").className === "search-button") {
       document.getElementById("anytime").className = "search-button-active"
@@ -41,28 +50,41 @@ class SplashSearch extends React.Component {
     }
   };
 
-  submitSearch() {
-    location.href = '/#/sites'
+  submitSearch(e) {
+    // location.href = '/#/sites'
+    e.preventDefault();
+    this.props.history.push(`/sites/${this.state.searchValue}`);
+  }
+
+  update(field) {
+    return e => 
+      this.setState({
+        [field]: e.currentTarget.value
+      });
   }
 
   render(){
     return (
       <div className="search-box">
-        <input
-          type="text"
-          className="search-bar"
-          placeholder="Try Yosemite, Moab, anywhere you'd like to go..."
-        />
-        <div className="search-buttons">
-          <div className="search-button" id = "anytime" onClick= {this.flipCalendar}><FontAwesomeIcon icon="calendar-alt" /> Anytime</div>
-          <div className="search-button" id="campsite" onClick={this.flipCamp}><FontAwesomeIcon icon="campground" /> Campsites</div>
-          <div className="search-button" id="lodging" onClick={this.flipLodging}><FontAwesomeIcon icon="dog" /> Pets</div>
-          <div className="search-button" id="rv" onClick={this.flipRv}><FontAwesomeIcon icon="bus" /> RVs</div>
-          <div className="search-submit" onClick={this.submitSearch}>Search</div>
-        </div>
+      <form onSubmit = {this.submitSearch}>
+          <input
+            type="text"
+            className="search-bar"
+            placeholder="Try Yosemite, Moab, anywhere you'd like to go..."
+            onChange={this.update("searchValue")}
+          />
+          <div className="search-buttons">
+            <div className="search-button" id = "anytime" onClick= {this.flipCalendar}><FontAwesomeIcon icon="calendar-alt" /> Anytime</div>
+            <div className="search-button" id="campsite" onClick={this.flipCamp}><FontAwesomeIcon icon="campground" /> Campsites</div>
+            <div className="search-button" id="lodging" onClick={this.flipLodging}><FontAwesomeIcon icon="dog" /> Pets</div>
+            <div className="search-button" id="rv" onClick={this.flipRv}><FontAwesomeIcon icon="bus" /> RVs</div>
+            {/* <div className="search-submit">Search</div> */}
+            <input type="submit" value="Search" className="search-submit"/>
+          </div>
+        </form>
       </div>
     );
   }
 }
 
-export default SplashSearch;
+export default withRouter(SplashSearch);
